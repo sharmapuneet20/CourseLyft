e498b94975a0107835cfabca2365721ae4b738b0
Puneet Sharma <puneetsharma200332@gmail.com>
2025-08-22 17:44:04 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,63 @@
+const {Schema, default: mongoose} = require("mongoose");
+  
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+     email: {type: String , unique:true },
+     password: String,
+     FirstName: String,
+     LastName: String,
+});
+
+
+
+const adminSchema = new Schema({
+    email: {type: String , unique:true },
+     password: String,
+     FirstName: String,
+     LastName: String,
+
+
+});
+
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+
+
+});
+
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+
+
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+
+
+module.exports = {
+    userModel: userModel,
+    adminModel: adminModel,
+    courseModel: courseModel,
+    purchaseModel: purchaseModel
+}
+  
+
+
+
+
+
+
+
+
